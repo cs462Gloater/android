886a4e38acc18d381219b459e0048d58c9be9009
Adam Nelson <togic8@gmail.com>
2015-04-12 00:28:06 -0600

    added the public IP address to the constants file

--- app/src/main/java/com/cs462/gloater/Constants.java
+++ app/src/main/java/com/cs462/gloater/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final int PLAYER_STAT_SUMMARY_TYPE_UNRANKED = 4;
 
-    public static final String AWS_PUBLIC_IP_ADDRESS = "";
+    public static final String AWS_PUBLIC_IP_ADDRESS = "54.152.227.132:3000";
 
     /**
      *  DO NOT COMMIT ACTUAL VALUES BELOW THIS LINE! EACH STRING SHOULD BE "SECRETn";
